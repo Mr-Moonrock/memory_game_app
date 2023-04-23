40a534f39f550da217edef70ed695fafb8bd27a1
Mr-Moonrock <rstivey35@gmail.com>
2023-04-23 01:33:51 -0400

    adjusted format

--- memorygame.js
+++ memorygame.js
@@ -4,78 +4,69 @@ const attemptsCounter = document.querySelector('span');
 let attempts = 0;
 
 attemptsCounter.textContent = attempts;
-             //Add Puzzle Pieces
+//Add Puzzle Pieces
     const cardList = () => [
         {name: 'Dixie.png', imgSrc: '/Users/maria/Desktop/MemGameIMG/Dixie.png'},
-
         {name: "Dixie.png", imgSrc: "/Users/maria/Desktop/MemGameIMG/Dixie.png"},
 
         {name: "Fiat.png", imgSrc: "/Users/maria/Desktop/MemGameIMG/Fiat.png"},
-
         {name: "Fiat.png", imgSrc: "/Users/maria/Desktop/MemGameIMG/Fiat.png"},
 
         {name: "FiatTopolino.png", imgSrc: "/Users/maria/Desktop/MemGameIMG/FiatTopolino.png"},
-
         {name: "FiatTopolino.png", imgSrc: "/Users/maria/Desktop/MemGameIMG/FiatTopolino.png"},
 
         {name: "NewBeetle.png", imgSrc: "/Users/maria/Desktop/MemGameIMG/NewBeetle.png"},
-
         {name: "NewBeetle.png", imgSrc: "/Users/maria/Desktop/MemGameIMG/NewBeetle.png"},
 
         {name: "OldBettle.png", imgSrc: "/Users/maria/Desktop/MemGameIMG/OldBeetle.png"},
-
         {name: "OldBettle.png", imgSrc: "/Users/maria/Desktop/MemGameIMG/OldBeetle.png"},
 
         {name: "VWVan.png", imgSrc: "/Users/maria/Desktop/MemGameIMG/VWVan.png"},
-
         {name: "VWVan.png", imgSrc: "/Users/maria/Desktop/MemGameIMG/VWVan.png"},
 
         {name: "YellowConvert.png", imgSrc: "/Users/maria/Desktop/MemGameIMG/YellowConvert.png"},
-
         {name: "YellowConvert.png", imgSrc: "/Users/maria/Desktop/MemGameIMG/YellowConvert.png"},
     ];
-            //Randomize Cards
+//Randomize Cards
 const randomize = () => {
-    const cardInfo = cardList();
-    cardInfo.sort(() => Math.random() - 0.5);
+const cardInfo = cardList();
+cardInfo.sort(() => Math.random() - 0.5);
     return cardInfo;
 };
-
-            //Card Generator Function
+//Card Generator Function
+//Generate HTML
+//Attach info to cards
+//Attach Card to Section
 const cardGenerator = () => {
-    const cardInfo = randomize ();
-
-    //Generate HTML
+const cardInfo = randomize ();
     cardInfo.forEach((item) => {
-        const card = document.createElement('div');
-        const face = document.createElement('img')
-        const back = document.createElement('div');
-        card.classList = 'card';
-        face.classList = 'face';
-        back.classList = 'back'; 
-        //Attach info to cards
-        face.src = item.imgSrc
-        card.setAttribute('name', item.name);
-        //Attach Card to Section
-        section.appendChild(card);
-        card.appendChild(face);
-        card.appendChild(back);
-
-        card.addEventListener('click', (e) => {
-            card.classList.toggle('toggleCard');
-            checkMatch(e);
+const card = document.createElement('div');
+const face = document.createElement('img')
+const back = document.createElement('div');
+    card.classList = 'card';
+    face.classList = 'face';
+    back.classList = 'back'; 
+    face.src = item.imgSrc
+    card.setAttribute('name', item.name);
+    section.appendChild(card);
+    card.appendChild(face);
+    card.appendChild(back);
+card.addEventListener('click', (e) => {
+    card.classList.toggle('toggleCard');
+    checkMatch(e);
         });
     });    
 };
-    //Check for Match
+//Check for Match
 const checkMatch = (e) => {
     console.log(e);
-    const clickedCard = e.target;
+const clickedCard = e.target;
     clickedCard.classList.add('flipped');
-    const flippedCards = document.querySelectorAll('.flipped');
-    const toggleCard = document.querySelectorAll('.toggleCard');
+const flippedCards = document.querySelectorAll('.flipped');
+const toggleCard = document.querySelectorAll('.toggleCard');
     console.log(flippedCards);
-    //Logic
+    
+//Logic
     if (flippedCards.length === 2) {
         if (
             flippedCards[0].getAttribute('name') === 
@@ -101,12 +92,12 @@ const checkMatch = (e) => {
             }
         }
     }
-    //Did we win?
+//Did we win?
     if (toggleCard.length === 14) {
         restart('You Won!');
     }
 };
-        //Restart
+//Restart
 const restart = (text) => {
     let cardInfo = randomize();
     let faces = document.querySelectorAll('.face');
@@ -125,7 +116,5 @@ const restart = (text) => {
     attemptsCounter.textContent = attempts;
     setTimeout(() => window.alert(text), 100);
 };
-
 cardGenerator();          
-
 });
